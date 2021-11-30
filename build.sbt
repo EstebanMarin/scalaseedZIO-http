64b7e435d66e07a968666ab9a45c6004e3976bba
esteban <esteban.marin@adidas.com>
2021-11-29 19:22:53 -0500

    code 302

--- build.sbt
+++ build.sbt
@@ -8,7 +8,11 @@ ThisBuild / organizationName := "example"
 lazy val root = (project in file("."))
   .settings(
     name := "zio-http-clean-test",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      // main dependencies
+      "dev.zio" %% "zio"   % "1.0.12",
+      "io.d11"  %% "zhttp" % "1.0.0.0-RC17"
+    )
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,21 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import zhttp.http.Header
+import zhttp.service.{ ChannelFactory, Client, EventLoopGroup }
+import zio.{ App, ExitCode, URIO, console }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+object SimpleClient extends App {
+  val env     = ChannelFactory.auto ++ EventLoopGroup.auto()
+  val url     = "http://sports.api.decathlon.com/groups/water-aerobics"
+  val headers = List(Header.host("sports.api.decathlon.com"))
+
+  val program                                                    = for {
+    res <- Client.request(url, headers)
+    _   <- console.putStrLn {
+             s"Code= ${res.status.toJHttpStatus.code()}"
+           }
+  } yield ()
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = program.exitCode.provideCustomLayer(env)
+
 }
